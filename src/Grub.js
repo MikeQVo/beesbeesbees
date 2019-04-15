7be4a56a7d27d493f1582346d546fd0c3be7c503
MikeQVo <angelictide@gmail.com>
2019-04-15 10:11:39 -0600

    provided working solution for beesbeesbees

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(food) {
+  if(food === 'jelly'){
+    return (this + ' eats the jelly');
+  } else {
+    return (this + ' does not eat the ' + food);
+  }
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, job, color, food) {
+  Bee.call(this, age, job, color, food);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = 'yellow';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+
+
 
